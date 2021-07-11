bd97559780978d938ce010d9743a494e12dda9f6
psick22 <kgirok@gmail.com>
2021-07-11 18:20:50 +0900

    init

---
+++ programmers/마라톤.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+def solution(participant, completion):
+    temp = Counter(participant) - Counter(completion)
+    answer = list(temp.keys())[0]
+    return answer
+
+
+
+
+solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
